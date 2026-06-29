47e6aac4aeafc4d27cb9107b12f91026a3810015
Sudarshan Pant <sudarshan.pant@gmail.com>
2026-06-29 11:30:53 +0100

    CORS  middleware setting in django, allow front end for CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -38,12 +38,14 @@ INSTALLED_APPS = [
     "django.contrib.gis",
     "rest_framework",
     "drf_spectacular",
+    "corsheaders",
     "stations",
     "forecasts",
     "core",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -51,6 +53,11 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 ROOT_URLCONF = "config.urls"
